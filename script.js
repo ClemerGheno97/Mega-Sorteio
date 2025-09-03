599a61a7b41b5201af30897524ccbc2a65ad4b7f
ClemerGheno97 <clemergheno97@gmail.com>
2025-09-03 20:07:54 -0300

    Melhorias no JS

--- script.js
+++ script.js
@@ -3,12 +3,18 @@ function draw() {
     const min = parseInt(document.querySelector(".min").value);
     const max = parseInt(document.querySelector(".max").value);
 
-    const numerosAleatorios = [];
-    for (let i = 0; i < luckynumbers; i++) {
-        const result = Math.floor(Math.random() * (max - min + 1) + min);
-        numerosAleatorios.push(result);
+
+    if (min >= max) {
+        alert("O valor mínimo deve ser menor que o valor máximo.")
     }
+    else {
+        const numerosAleatorios = [];
+        for (let i = 0; i < luckynumbers; i++) {
+            const result = Math.floor(Math.random() * (max - min + 1) + min);
+            numerosAleatorios.push(result);
+        }
 
-    alert(`Os números sorteados foram: ${numerosAleatorios.join(", ")}`);
+        alert(`Os números sorteados foram: ${numerosAleatorios.join(", ")}`);
 
+    }
 }
